e0f77662a009c17867da8d2a594bdd704385c7c5
Srivani Katta <kattasrivani74@gmail.com>
2026-04-20 00:39:36 +0530

    Updated API utility configuration

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://hotelbooking-backend-1-texw.onrender.com/api",
   timeout: 10000,
 });
 
